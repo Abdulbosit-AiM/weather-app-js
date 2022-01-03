6854392b9762ee54fd3367f529b19c79b4f33aa0
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-01-03 20:15:18 +0500

    init

---
+++ script.js
@@ -0,0 +1,49 @@
+let weather  = {
+    'apiKey': '8cf1a897d9da6aa0a705c0605e394b86',
+    fetchWeather: function(city){
+        fetch(
+            'api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + this.apiKey
+        ).then((response) => {
+            if (!response.ok) {
+              alert("No weather found.");
+              throw new Error("No weather found.");
+            }
+            return response.json();
+          })
+          .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data
+        const { icon, description } = data.weather[0]
+        const { temp, humidity } = data.main
+        const { speed } = data.wind
+
+        document.querySelector('.city').innerText = 'Weather in ' + name
+        document.querySelector('.icon').src = ' https://openweathermap.org/img/wn/' + icon + '.png'
+        document.querySelector('.description').innerText = description
+        document.querySelector('.temp').innerText = temp + '° C'
+        document.querySelector('.humidity').innerText = humidity + '%'
+        document.querySelector('.wind').innerText = 'Wind Speed: ' + speed + 'km/h'
+
+        document.querySelector('.weather').classList.remove('loading')
+        document.body.style.backgroundImage = 'url("https://source.unsplash.com/1600x900/?' + name + '")'
+
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector('.search-bar').value)
+    }
+}
+
+// https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=8cf1a897d9da6aa0a705c0605e394b86
+
+document.querySelector('.search button').addEventListener('click', function() {
+    weather.search()
+})
+
+document.querySelector('.search-bar').addEventListener('keyup', function(event) {
+    if (event.key == 'Enter') {
+        weather.search()
+    }
+})
+
+weather.fetchWeather('Denver')
