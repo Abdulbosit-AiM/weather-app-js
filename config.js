d368dd12ecf40465d0e50e75f8bdf8c9724981a3
A.B. [Abdulbosit] <abdulbosit.melikuziev@gmail.com>
2025-04-25 08:56:44 +0100

    fix back

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 var config = {
-    SECRET_API_KEY : '6WkK4x_SRvlHrIFrJk7fCLbNuvBL806Fmg-l02KUpqk',
+    SECRET_API_KEY : '8cf1a897d9da6aa0a705c0605e394b86',
   }
+
+  // Unsplash API: 6WkK4x_SRvlHrIFrJk7fCLbNuvBL806Fmg-l02KUpqk
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather  = {
-    'apiKey': config.SECRET_API_KEY,
+    'apiKey': "config.SECRET_API_KEY",
     fetchWeather: function (city) {
         fetch(
             'https://api.openweathermap.org/data/2.5/weather?q=' 
